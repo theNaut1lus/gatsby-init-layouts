17a1b545265ca43625bae322c6a1f82679b9b3ee
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-04-10 12:51:46 +0530

    step 3: layouts done

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>About me</h1>
+        <p>
+            Hello, Bye and i hate CSE still i love React. 
+        </p>
+    </Layout>
+)
